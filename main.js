d3ee6a21141fd2b343d1b47e0aaef9256b2aebd2
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-09-12 02:13:31 +0530

    routes added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-// import router from './router';
+import router from './router';
 import {store} from './store';
 let fb = require('./firebaseConfig');
 Vue.config.productionTip = false
@@ -14,7 +14,12 @@ fb.auth.onAuthStateChanged(user => {
         app = new Vue({
             el: '#app',
             store,
+            router,
             render: h => h(App)
         })
     }
 })
+
+// Check if the route exists and requires authentication.
+// Then create a reference to the current user and authenticate routes.
+
